3ed1a2fb0f1f53d3d7c73cf82690f26128d512a6
Petretto <pczjbp@gmail.com>
2025-05-11 11:51:27 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,3 @@
-# from .user import User
-# from .job_offer import JobOffer, OfferSkill
-# from .application import Application
-# from .skill import Skill
-# from .certification import Certification
-# from .invitation import Invitation
-
 from .user import User
 from .job_offer import JobOffer
 from .offer_skill import OfferSkill
